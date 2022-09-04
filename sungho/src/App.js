e5b85fc233bf7e31c9fc2f2eff3c4557650bdf48
kimsungho <ksh_1035@naver.com>
2022-09-05 04:18:34 +0900

    상세 모달창 완성!

--- sungho/src/App.js
+++ sungho/src/App.js
@@ -6,7 +6,9 @@ import { useState } from 'react';
 function App() {
 
   let [post, setPost] = useState(["남자 코트 추천", "여자 코트 추천", "강남 우동 맛집"]);
-  let [like, setLike] = useState(0)
+  let [like, setLike] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   return (
     <div className="App">
@@ -25,36 +27,42 @@ function App() {
         </div>
       </div>
 
-      <div className="postList">
-        <div className='postMenu'>
-          <button onClick={ () => {
-            let copy = [...post];
-            copy.sort();
-            setPost(copy)
-          }}>정렬</button>
-        </div>
-        <h4>{post[0]} 
-          <div className='postLike'>
-            <span onClick={ () => {setLike( like + 1)} }>Like</span> {like} 
-          </div>
-        </h4>
-        <p>2022년 9월 2일</p>
-        <h4>{post[1]}
-          <div className='postLike'>
-            <span onClick={ () => {setLike( like + 1)} }>Like</span> {like} 
-          </div>
-        </h4>
-        <p>2022년 9월 2일</p>
-        <h4>{post[2]}
-          <div className='postLike'>
-            <span onClick={ () => {setLike( like + 1)} }>Like</span> {like} 
-          </div>
-        </h4>
-        <p>2022년 9월 2일</p>
-      </div>
+      {
+        post.map(function(post, i){
+          return (
+            <div className="postList" key={i}>
+              <h4 onClick={ () => {setModal(true), setTitle(i)}}>{ post }</h4>
+                <div className='postLike' onClick={ () => {
+                      let likeCnt = [...like];
+                      likeCnt[i]++;
+                      setLike(likeCnt);
+                    } 
+                  }>
+                  <span>Like</span> {like[i]} 
+                </div>
+              <p>2022년 9월 2일</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal post={post} title={title}/> : null
+      }
 
     </div>
   );
+}
+
+function Modal(props) {
+  return(
+    <div className='modal'>
+      <a class="close" onClick={ () => {setModal(false)}}>&times;</a>
+      <h4>{ props.post[props.title] }</h4>
+      <p>Date</p>
+      <p>content</p>
+    </div>
+  )
 }
 
 export default App;
